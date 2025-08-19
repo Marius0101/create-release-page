c77eca8e91702ca29c42793be0217fe6b54a2871
Marius0101 <mariusoctavian010@gmail.com>
2025-08-19 17:48:18 +0300

    add method to get the change log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+import * as github from "@actions/github";
+import * as core from "@actions/core";
 
 const run = async (): Promise<void> =>{
-
+    //const inputs = await getInputs();
+    //const octokit = github.getOctokit(inputs.ghToken);
+    const octokit = github.getOctokit("your token");
+    const changeLogContent: string = await getChangeLogContent(octokit, "CHANGELOG.md", "v1" );
 }
+
+const getChangeLogContent = async (octokit: any, filePath: string, ref: string): Promise<string> => {
+    core.info(`Trying to get ${filePath} from the ref ${ref}`)
+    const response = octokit.rest.repos.getContent({
+        owner: 'Marius0101',
+        repo: 'create-pull-request',
+        path: filePath,
+        ref: ref
+    });
+    if(!response.content){
+        throw new Error("Unexpected response structure or content missing.");
+    }
+    core.info(`File content get succesfuly`);
+    core.info(`Start decoding the content from base64`);
+    const content = Buffer.from(response.content, 'base64').toString('utf-8');
+    core.info(`Content decoded succesfuly`);
+    return content;
+};
+
 
 export default run;
