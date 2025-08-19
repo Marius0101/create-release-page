d9b183849076f182f017b065ecbbd3aa3f349a23
Marius0101 <mariusoctavian010@gmail.com>
2025-08-19 17:05:01 +0300

    clean code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import * as github from "@actions/github";
-import { createReleasePage, getChangeLog, getInputs } from "./common";
 
 const run = async (): Promise<void> =>{
-  const inputs = await getInputs();
-  const octokit = github.getOctokit(inputs.token);
-  if(inputs.change_log_file){
-    inputs.body += await getChangeLog(inputs, octokit)
-  }
-  await createReleasePage(inputs,octokit);
+
 }
+
 export default run;
